7260d0fed05261b53686407aebf0c43a2c93e157
Sebastian Velasco Borrero <sebasvelasco353@hotmail.com>
2020-07-11 19:35:54 -0500

    WIP

--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 
+// img
+import clouds from '../../assets/img/cloud.svg'
+import water from '../../assets/img/water.svg'
+import drizzle from '../../assets/img/weather.svg'
+import sun from '../../assets/img/sun.svg'
+
 const StyledForecast = styled.div`
     padding: 4rem 1rem;
     display: flex;
@@ -8,13 +14,40 @@ const StyledForecast = styled.div`
 `;
 
 export default class Forecast extends Component {
+
+    weatherImage(){
+        switch (this.state.cityWeather.weather[0].main) {
+            case "Clouds":
+                return <img style={{ width: '200px' }} src={clouds} alt="Clouds"/>
+            case "Drizzle":
+                return <img style={{ width: '200px' }} src={drizzle} alt="Clouds"/>
+            case "Rain":
+                return <img style={{ width: '200px' }} src={water} alt="Clouds"/>    
+            default:
+                return <img style={{ width: '200px' }} src={sun} alt="Clouds"/>
+        }
+    }
     render() {
         return (
             <StyledForecast>
-                <h4>fecha</h4>
-                {/* <img src="" alt="" srcset=""/> */}
-                <h3>temperatura C</h3>
-                <p>estado</p>
+                {this.props.cityWeather ? (
+                    <div className="dayWeather">
+                        <div className="imgPlaceholder">
+                            {/* img placeholder */}
+                            {this.weatherImage()}
+                        </div>
+                        <p>
+                            <b>{this.props.cityWeather.weather[0].description}</b> 
+                            <span className="temp_max">{this.props.cityWeather.main.temp_max + " °C."}</span> 
+                            <span className="temp_min">{this.props.cityWeather.main.temp_min + " °C."}</span> 
+                            <span className="humedad"> Humedad: {this.props.cityWeather.main.humidity}%</span> | 
+                            <span className="vientos"> Vientos: {this.props.cityWeather.wind.speed}m/s</span>
+                        </p>
+                        <p>
+                            Icons downloaded from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+                        </p>
+                    </div>
+                ) : null}
             </StyledForecast>
         )
     }
--- src/pages/Recent/Recent.js
+++ src/pages/Recent/Recent.js
@@ -1,11 +1,87 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+// Components
+import Searchbox from '../../components/Searchbox/Searchbox'
+import Forecast from '../../components/Forecast/Forecast';
+
+const StyledHome = styled.div`
+    padding: 50px 50px;
+    .dayWeather{
+        margin: 0rem auto;
+        width: 60%;
+        height: 40vh;
+        display: flex;
+        flex-direction: column;
+        font-weight: bold;
+        .imgPlaceholder{
+            height: 30vh;
+            background-color: transparent;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+        p{
+            border-top: solid 3px gray;
+            margin-top: 20px;
+            padding-top: 20px;
+            text-align: center;
+            .temp_max{
+                padding: 0.5rem 1.5rem;
+                margin: 0.5rem;
+                background-color: rgb(72, 149, 185);
+                color: white;
+            }
+            .temp_min{
+                padding: 0.5rem 1.5rem;
+                margin: 0.5rem;
+                background-color: black;
+                color: white;
+            }
+        }
+    }
+`;
 
 export default class Recent extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            city: "Bogota",
+            cityWeather: null
+        }
+    }
+    componentDidMount() {
+        console.log("holi");
+        this.searchWeather();
+    }
+    capitalizeSearch = (str) => {
+        if(typeof str === 'string') {
+            return str.replace(/^\w/, c => c.toUpperCase());
+        } else {
+            return '';
+        }
+    };
+    searchWeather(){
+        if (this.state.city !== "" && this.state.city !== null && this.state.city !== undefined) {
+            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.capitalizeSearch(this.state.city)}&units=metric&APPID=71fddc04d2f1bb3ccf0749934ed9b816`)
+            .then(res => res.json())
+            .then((data) => {
+                console.log(data);
+                this.setState({
+                    cityWeather: data
+                });
+            }).catch(console.log);
+        } else {
+            alert("Porfavor ingresa una ciudad");
+        }
+    };
+    handleSearchboxChange(e){
+        this.setState({ city: e.target.value });
+    };
     render() {
         return (
-            <div>
-                holkfnovneowifwdfnein
-            </div>
+            <StyledHome>
+                <Forecast info={this.state.cityWeather} />
+            </StyledHome>
         )
     }
 }
